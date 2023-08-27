a2140a888197d2acc406397ac0ba7dae08de9d88
Evelyn Matos <matos.evelyn1.8@gmail.com>
2023-08-26 21:12:53 -0300

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/64/60df55166b44001e1b5ca3c40d35740a
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author evelyn
+ *
+ */
+module DesafioControleFluxo {
+}
---
+++ DesafioControleFluxo/src/application/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package application;
+
+public class ParametrosInvalidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
